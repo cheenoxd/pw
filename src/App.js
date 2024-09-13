5976ac9057161ec9be905c7f8495cf37d95e2d06
Zesan <zesanrahim@gmail.com>
2024-09-13 13:48:33 -0400

    second

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home'
+import Nabar from './components/Navbar'
+
+
+export default function App() 
+return {
+  <Router>
+    <div>
+      
+    </div>
+
+  </Router>
+    
+
+
+
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './css/Navbar.css'
+
+export default function Navbar () {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen); 
+  };
+
+  return (
+    <nav>
+      
+    </nav>
+
+  )
+
+  
+
+
+} 
